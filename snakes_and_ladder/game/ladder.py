8b2b47f6bed8757ef02dc930c274eb9a2e776e73
Sai Kumar Kundlapelli <saikumarkundlapelli@gmail.com>
2025-01-10 10:42:15 +0530

    roll back with user change

--- snakes_and_ladder/game/ladder.py
+++
@@ -1,4 +0,0 @@
-class Ladder:
-    def __init__(self, from_cell: int, to_cell: int):
-        self.from_cell: int = from_cell
-        self.to_cell: int = to_cell
--- snakes_and_ladder/game/player.py
+++
@@ -1,5 +0,0 @@
-class Player:
-    def __init__(self, name: str, id: int):
-        self.name = name
-        self.id = id
-        self.position = 1
